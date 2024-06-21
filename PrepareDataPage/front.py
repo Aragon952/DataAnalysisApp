0da46d36e3a488d2a2ef0a8b85a0e559f19add51
Mihai <petrovmihai952@gmail.com>
2024-06-22 01:49:29 +0300

    ProgresBunPrimeleFunctiiPrepareFaraProbleme

--- PrepareDataPage/front.py
+++ PrepareDataPage/front.py
@@ -1,17 +1,22 @@
 import tkinter as tk
 from tkinter import ttk
 from AnalyzeData.front import open_analyze_data_page
-from PrepareDataPage.functions import update_entry_and_methods, save_csv, apply_method
+from PrepareDataPage.functions import *
 
-numeric_values_methods_list = ['Selectare Coloane','FillNa cu mediana', 'FillNa cu medie', 'FillNa cu valoare specifică', 
-                               'MICE', 'Replace', 'Standardizare', 'Normalizare', 'Tratare outlieri', 'Grupare', 
-                               'Redenumire coloană', 'Eliminare coloană']
-alfanumeric_values_methods_list = ['Selectare Coloane', 'FillNa cu valoare specifică', 'Replace','String Slicing', 
-                                   'Codare binara pe pozitii', 'Codificare numerica', 'Grupare', 'Redenumire coloană', 'Eliminare coloană']
+numeric_methods_all_columns = ['Selectare Coloane','FillNa cu mediana', 'FillNa cu medie', 'FillNa cu valoare specifică', 
+                               'MICE', 'Standardizare', 'Normalizare', 'Tratare outlieri', 'Grupare', 'Eliminare coloane']
+numeric_methods_one_column = ['Replace', 'Redenumire coloana', 'Eliminare coloana', 'FillNa cu mediana', 'FillNa cu medie', 
+                              'FillNa cu valoare specifică', 'MICE', 'Standardizare', 'Normalizare', 'Tratare outlieri']
+alfanumeric_methods_all_columns = ['FillNa cu valoare specifică', 'String Slicing', 'Codare binara pe pozitii', 
+                                   'Codificare numerica', 'Grupare', 'Eliminare coloane']
+alfanumeric_methods_one_column = ['FillNa cu valoare specifică', 'Replace', 'String Slicing', 'Codare binara pe pozitii', 
+                                  'Codificare numerica', 'Redenumire coloană', 'Eliminare coloană']
 
 def open_prepare_data_page(user_id, dataframe):
     def on_closing():
         prepare_window.destroy()
+    
+    dataframe_container = {"dataframe": dataframe}
 
     prepare_window = tk.Tk()  # Create a new Tk instance instead of Toplevel
     prepare_window.title("Preparare Date")
@@ -24,10 +29,10 @@ def open_prepare_data_page(user_id, dataframe):
     tree_frame.pack(fill=tk.BOTH, expand=True, pady=20, padx=20)  # Ensure padding to see the frame's extent
 
     # TreeView setup
-    main_tree = ttk.Treeview(tree_frame, columns=dataframe.columns.tolist(), show="headings")
-    for col in dataframe.columns:
+    main_tree = ttk.Treeview(tree_frame, columns=dataframe_container["dataframe"].columns.tolist(), show="headings")
+    for col in dataframe_container["dataframe"].columns:
         main_tree.heading(col, text=col)
-        main_tree.column(col, width=100)  # You may need to adjust widths based on actual content
+        main_tree.column(col, width=100)
     main_tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
     # Vertical Scrollbar
@@ -41,7 +46,7 @@ def open_prepare_data_page(user_id, dataframe):
     main_tree.configure(xscrollcommand=hsb.set)
 
     # Ensure data is loaded into the TreeView
-    for index, row in dataframe.iterrows():
+    for index, row in dataframe_container["dataframe"].iterrows():
         main_tree.insert("", tk.END, values=list(row))
 
     # Listbox Frame for numeric and alphanumeric columns
@@ -52,22 +57,48 @@ def open_prepare_data_page(user_id, dataframe):
     ttk.Label(list_frame, text="Coloane numerice").pack(side=tk.LEFT, padx=10)
     num_listbox = tk.Listbox(list_frame, height=5)
     num_listbox.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.X, expand=True)
-    num_methods = ttk.Combobox(list_frame, values=numeric_values_methods_list, width=25)
+    num_methods = ttk.Combobox(list_frame, values=numeric_methods_all_columns, width=25)
     num_methods.pack(side=tk.LEFT, padx=10)
-    ttk.Button(list_frame, text="Folosește metoda pe\ncoloanele numericele", command=lambda: apply_method(dataframe, num_methods.get(), 'numeric', main_tree)).pack(side=tk.LEFT, padx=10)
+
+    def handle_numeric_all_method_selection(event):
+        selected_method = num_methods.get()
+        if selected_method == 'Selectare Coloane':
+            select_columns(dataframe_container["dataframe"], tree_frame, dataframe_container, num_listbox, alpha_listbox)
+        elif selected_method == 'FillNa cu mediana':
+            fill_na_with_median(dataframe_container, tree_frame)
+        elif selected_method == 'FillNa cu medie':
+            fill_na_with_mean(dataframe_container, tree_frame)
+        elif selected_method == 'FillNa cu valoare specifică':
+            fill_na_with_specific_value(dataframe_container, tree_frame)
+        elif selected_method == 'MICE':
+            perform_mice_imputation(dataframe_container, tree_frame)
+        elif selected_method == 'Standardizare':
+            perform_standardization(dataframe_container, tree_frame)
+        elif selected_method == 'Normalizare':
+            perform_normalization(dataframe_container, tree_frame)
+        elif selected_method == 'Tratare outlieri':
+            handle_outliers(dataframe_container, tree_frame)
+        elif selected_method == 'Grupare':
+            perform_grouping(dataframe_container, tree_frame)
+        elif selected_method == 'Eliminare coloane':
+            remove_columns(dataframe_container, tree_frame)
+        else:
+            print("Method not implemented yet")
+
+    num_methods.bind('<<ComboboxSelected>>', handle_numeric_all_method_selection)
 
     # Alphanumeric columns setup
     ttk.Label(list_frame, text="Coloane alfanumerice").pack(side=tk.LEFT, padx=10)
     alpha_listbox = tk.Listbox(list_frame, height=5)
     alpha_listbox.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.X, expand=True)
-    alpha_methods = ttk.Combobox(list_frame, values=alfanumeric_values_methods_list, width=25)
+    alpha_methods = ttk.Combobox(list_frame, values=alfanumeric_methods_all_columns, width=25)
     alpha_methods.pack(side=tk.LEFT, padx=10)
-    ttk.Button(list_frame, text="Aplica metoda pe\ncoloanele alfanumerice", command=lambda: apply_method(dataframe, alpha_methods.get(), 'alpha', main_tree)).pack(side=tk.LEFT, padx=10)
+    ttk.Button(list_frame, text="Aplica metoda pe\ncoloanele alfanumerice").pack(side=tk.LEFT, padx=10)
 
     # Populate listboxes
-    for col in dataframe.select_dtypes(include='number').columns:
+    for col in dataframe_container["dataframe"].select_dtypes(include='number').columns:
         num_listbox.insert(tk.END, col)
-    for col in dataframe.select_dtypes(exclude='number').columns:
+    for col in dataframe_container["dataframe"].select_dtypes(exclude='number').columns:
         alpha_listbox.insert(tk.END, col)
 
     # Entry and method selection based on column type
@@ -76,19 +107,22 @@ def open_prepare_data_page(user_id, dataframe):
 
     method_cb = ttk.Combobox(main_frame)
     method_cb.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.X, expand=True)
-    ttk.Button(main_frame, text="Folosește metoda pentru coloană", command=lambda: apply_method(entry.get(), method_cb.get(), dataframe)).pack(side=tk.LEFT, padx=10, pady=10)
+    ttk.Button(main_frame, text="Folosește metoda pentru coloană").pack(side=tk.LEFT, padx=10, pady=10)
 
     # Bind listbox selection changes
-    num_listbox.bind('<<ListboxSelect>>', lambda event: update_entry_and_methods(num_listbox, entry, method_cb, ['Normalizează', 'Standardizează']))
-    alpha_listbox.bind('<<ListboxSelect>>', lambda event: update_entry_and_methods(alpha_listbox, entry, method_cb, ['Codifică', 'Umple goluri']))
+    num_listbox.bind('<<ListboxSelect>>', lambda event: update_methods_for_one_column(num_listbox, entry, method_cb, numeric_methods_one_column))
+    alpha_listbox.bind('<<ListboxSelect>>', lambda event: update_methods_for_one_column(alpha_listbox, entry, method_cb, alfanumeric_methods_one_column))
 
     # Analyze button
-    execute_button = ttk.Button(main_frame, text="Analizeaza datele", command=lambda: open_analyze_data_page(user_id, dataframe, prepare_window))
+    execute_button = ttk.Button(main_frame, text="Analizeaza datele", command=lambda: open_analyze_data_page(user_id, dataframe_container["dataframe"], prepare_window))
     execute_button.pack(padx=10, pady=10, fill=tk.X, expand=False)
 
     # Save data button
-    save_button = ttk.Button(main_frame, text="Salvează datele", command=lambda: save_csv(dataframe, user_id))
+    save_button = ttk.Button(main_frame, text="Salvează datele", command=lambda: save_csv(dataframe_container, user_id))
     save_button.pack(padx=10, pady=10, fill=tk.X, expand=False)
+
+    ttk.Button(list_frame, text="Select Columns", command=lambda: select_columns(dataframe_container["dataframe"], main_tree, dataframe_container)).pack(side=tk.LEFT, padx=10)
+
 
     prepare_window.protocol("WM_DELETE_WINDOW", on_closing)
     prepare_window.mainloop()
--- PrepareDataPage/functions.py
+++ PrepareDataPage/functions.py
@@ -3,21 +3,42 @@ from tkinter import simpledialog, messagebox, ttk
 import os
 import sqlite3
 
-def update_entry_and_methods(listbox, entry, method_cb, methods):
+def update_methods_for_one_column(listbox, entry, method_cb, methods):
     selection = listbox.curselection()
     if selection:
         entry.delete(0, tk.END)
         entry.insert(0, listbox.get(selection[0]))
         method_cb['values'] = methods  # Update the methods in the combobox
 
-def update_treeview(dataframe, tree):
-    tree.configure(columns=dataframe.columns.tolist())
+def update_treeview(dataframe, tree_frame):
+    # Îndepărtăm orice widgets existente din tree_frame
+    for widget in tree_frame.winfo_children():
+        widget.destroy()
+
+    # Creăm un nou Treeview în tree_frame
+    main_tree = ttk.Treeview(tree_frame, columns=dataframe.columns.tolist(), show="headings")
+
+    # Setăm antetele și lățimea coloanelor
     for col in dataframe.columns:
-        tree.heading(col, text=col)
-        tree.column(col, width=100)
-    tree.delete(*tree.get_children())
+        main_tree.heading(col, text=col)
+        main_tree.column(col, width=100)
+
+    # Adăugăm un scrollbar vertical
+    vsb = ttk.Scrollbar(tree_frame, orient="vertical", command=main_tree.yview)
+    vsb.pack(side='right', fill='y')
+    main_tree.configure(yscrollcommand=vsb.set)
+
+    # Adăugăm un scrollbar orizontal
+    hsb = ttk.Scrollbar(tree_frame, orient="horizontal", command=main_tree.xview)
+    hsb.pack(side='bottom', fill='x')
+    main_tree.configure(xscrollcommand=hsb.set)
+
+    # Adăugăm datele
     for index, row in dataframe.iterrows():
-        tree.insert("", tk.END, values=row.tolist())
+        main_tree.insert("", tk.END, values=list(row))
+
+    # Ambalăm main_tree în tree_frame
+    main_tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
 def save_file_info_to_database(user_id, filename, filepath):
     conn = sqlite3.connect('DataAnalysisApp/database.db')
@@ -29,7 +50,8 @@ def save_file_info_to_database(user_id, filename, filepath):
     conn.commit()
     conn.close()
 
-def save_csv(dataframe, user_id):
+def save_csv(dataframe_container, user_id):
+    dataframe = dataframe_container["dataframe"]
     dialog_root = tk.Toplevel()
     dialog_root.withdraw()  # Hide the dialog main window
 
@@ -55,63 +77,55 @@ def save_csv(dataframe, user_id):
     
     dialog_root.destroy()
 
-def apply_method(dataframe, method, col_type, main_tree):
-    if col_type == 'numeric':
-        columns = dataframe.select_dtypes(include='number').columns
-    else:
-        columns = dataframe.select_dtypes(exclude='number').columns
-
-    for column in columns:
-        if method == 'Selectare Coloane':
-            select_columns(dataframe, column, main_tree)
-        elif method == 'FillNa cu mediana':
-            fill_na_with_median(dataframe, column)
-        elif method == 'FillNa cu medie':
-            fill_na_with_mean(dataframe, column)
-        elif method == 'FillNa cu valoare specifică':
-            value = get_value_from_user("Enter the value to fill NA:")
-            fill_na_with_value(dataframe, column, value)
-        elif method == 'MICE':
-            mice_imputation(dataframe, column)
-        elif method == 'Replace':
-            replace_values(dataframe, column)
-        elif method == 'Standardizare':
-            standardize(dataframe, column)
-        elif method == 'Normalizare':
-            normalize(dataframe, column)
-        elif method == 'Tratare outlieri':
-            handle_outliers(dataframe, column)
-        elif method == 'Grupare':
-            group_data(dataframe, column)
-        elif method == 'Redenumire coloană':
-            rename_column(dataframe, column)
-        elif method == 'Eliminare coloană':
-            drop_column(dataframe, column)
-        elif col_type == 'alpha' and method == 'String Slicing':
-            string_slicing(dataframe, column)
-        elif col_type == 'alpha' and method == 'Codare binara pe pozitii':
-            binary_encoding(dataframe, column)
-        elif col_type == 'alpha' and method == 'Codificare numerica':
-            numeric_encoding(dataframe, column)
-        else:
-            print(f"Method '{method}' is not recognized for columns of type '{col_type}'. No action taken.")
-
-    print(f"Applied {method} to all {col_type} columns: {', '.join(columns)}")
-    update_treeview(dataframe, main_tree)
-
-
 def get_value_from_user(prompt):
+    # Creează un dialog Toplevel
     dialog_root = tk.Toplevel()
-    dialog_root.grab_set()
-    dialog_root.withdraw()
-    value = simpledialog.askstring("Input", prompt, parent=dialog_root)
-    dialog_root.destroy()
-    return value
+    dialog_root.title("Input")
 
-def select_columns(dataframe, column, main_tree):
+    # Adaugă un Entry widget pentru a permite utilizatorului să introducă o valoare
+    entry = tk.Entry(dialog_root)
+    entry.pack(padx=20, pady=20)
+
+    # Funcția care va fi apelată la apăsarea butonului OK
+    def on_ok():
+        user_value = entry.get()  # Extrage valoarea introdusă de utilizator
+        dialog_root.user_value = user_value  # Stochează valoarea într-un atribut al fereastrei
+        dialog_root.destroy()  # Închide fereastra după ce valoarea a fost preluată
+
+    # Adaugă butoane pentru confirmare
+    ok_button = tk.Button(dialog_root, text="OK", command=on_ok)
+    ok_button.pack(pady=10)
+
+    # Rulează loop-ul principal al dialogului
+    dialog_root.mainloop()
+
+    # Returnează valoarea introdusă de utilizator după închiderea ferestrei
+    return dialog_root.user_value if hasattr(dialog_root, 'user_value') else None
+
+def update_listboxes(dataframe, num_listbox, alpha_listbox):
+    # Curățăm întâi conținutul actual al listbox-urilor
+    num_listbox.delete(0, tk.END)
+    alpha_listbox.delete(0, tk.END)
+
+    # Adăugăm coloanele noi în listbox-uri
+    # Identificăm coloanele numerice și alfanumerice
+    numeric_columns = dataframe.select_dtypes(include='number').columns
+    alfanumeric_columns = dataframe.select_dtypes(exclude='number').columns
+
+    # Actualizăm listbox-ul pentru coloanele numerice
+    for col in numeric_columns:
+        num_listbox.insert(tk.END, col)
+
+    # Actualizăm listbox-ul pentru coloanele alfanumerice
+    for col in alfanumeric_columns:
+        alpha_listbox.insert(tk.END, col)
+
+def select_columns(dataframe, tree_frame, dataframe_container, num_listbox, alpha_listbox):
     top = tk.Toplevel()
     top.title("Select Columns")
-    top.grab_set()
+
+    def on_closing():
+        top.destroy()
 
     main_frame = ttk.Frame(top, padding="3 3 12 12")
     main_frame.pack(fill=tk.BOTH, expand=True)
@@ -146,7 +160,9 @@ def select_columns(dataframe, column, main_tree):
     def save_changes():
         global original_dataframe
         original_dataframe = dataframe[selected_columns]
-        update_treeview(original_dataframe, main_tree)
+        dataframe_container['dataframe'] = original_dataframe
+        update_treeview(original_dataframe, tree_frame)
+        update_listboxes(original_dataframe, num_listbox, alpha_listbox)
         top.destroy()
 
     button_frame = ttk.Frame(main_frame)
@@ -166,6 +182,8 @@ def select_columns(dataframe, column, main_tree):
     main_frame.columnconfigure(2, weight=0)
     main_frame.rowconfigure(1, weight=1)
 
+    top.protocol("WM_DELETE_WINDOW", on_closing)
+
     top.mainloop()
 
 
@@ -179,14 +197,62 @@ def numeric_encoding(dataframe, column):
     pass
 
 # Funcțiile existente (asigură-te că toate sunt definite)
-def fill_na_with_median(dataframe, column):
-    dataframe[column].fillna(dataframe[column].median(), inplace=True)
+def fill_na_with_median(dataframe_container, tree_frame):
+    dataframe = dataframe_container["dataframe"]
+    numeric_columns = dataframe.select_dtypes(include=['number']).columns
+    for column in numeric_columns:
+        # Use .loc to ensure you are modifying the DataFrame directly
+        median_value = dataframe[column].median()
+        dataframe.loc[:, column].fillna(median_value, inplace=True)
+    update_treeview(dataframe, tree_frame)
 
-def fill_na_with_mean(dataframe, column):
-    dataframe[column].fillna(dataframe[column].mean(), inplace=True)
+def fill_na_with_mean(dataframe_container, tree_frame):
+    dataframe = dataframe_container["dataframe"]
+    numeric_columns = dataframe.select_dtypes(include=['number']).columns
+    for column in numeric_columns:
+        # Use .loc to ensure you are modifying the DataFrame directly
+        mean_value = dataframe[column].mean()
+        dataframe.loc[:, column].fillna(mean_value, inplace=True)
+    update_treeview(dataframe, tree_frame)
 
-def fill_na_with_value(dataframe, column, value):
-    dataframe[column].fillna(value, inplace=True)
+def fill_na_with_specific_value(dataframe_container, tree_frame):
+    # Creează o fereastră de top-level pentru dialog
+    top = tk.Toplevel()
+    top.title("Fill Missing Values")
+
+    # Setează fereastra să nu permită interacțiunea cu fereastra principală
+    top.grab_set()
+
+    # Textul de informare pentru utilizator
+    label = ttk.Label(top, text="Please enter a specific value to fill missing values in numeric columns:")
+    label.pack(padx=20, pady=10)
+
+    # Câmp pentru valoarea specificată de utilizator
+    value_entry = ttk.Entry(top)
+    value_entry.pack(padx=20, pady=10)
+
+    # Funcția care va fi apelată la apăsarea butonului 'OK'
+    def on_ok():
+        specific_value = value_entry.get()
+        try:
+            # Convertim valoarea primită într-un float
+            specific_value = float(specific_value)
+            # Aplicăm valoarea specifică pentru a umple valorile NaN în coloanele numerice
+            numeric_columns = dataframe_container["dataframe"].select_dtypes(include='number').columns
+            for column in numeric_columns:
+                dataframe_container["dataframe"][column].fillna(specific_value, inplace=True)
+            # Actualizăm interfața
+            update_treeview(dataframe_container["dataframe"], tree_frame)
+            top.destroy()
+        except ValueError:
+            ttk.Label(top, text="Invalid input, please enter a numeric value.").pack(padx=20, pady=20)
+
+    # Buton pentru confirmarea valorii
+    ok_button = ttk.Button(top, text="OK", command=on_ok)
+    ok_button.pack(pady=20)
+
+    # Rulează loop-ul principal al dialogului
+    top.mainloop()
 
 def mice_imputation(dataframe, column):
     pass
